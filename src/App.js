98f78191296441037582ffedfc9c552f35d76023
Sruthika A B <sruthikamurugan@gmail.com>
2024-03-20 12:08:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductList from './Fetch'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <ProductList />      
+    </div>
+  );
+};
+
+export default App;
